452c96f5c2688c6dce310abe9103af4d3399c0b2
KANIN SRIJUNDORN <kaninsrijundorn@KANINs-MacBook-Pro.local>
2020-05-05 16:24:48 +0700

    broke component down and style it up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import HomePage from "./hompeage.componet";
-
+import HomePage from "./pages/homepage/hompeage.componet";
 function App() {
   return (
     <div className="App">
