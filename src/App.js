c0aa79a3f10de15474aa55550ec8c023d4bf9407
KANIN SRIJUNDORN <kaninsrijundorn@MacBook-Pro.local>
2020-06-30 09:27:43 +0700

    auto commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/homepage/hompeage.componet';
 
-import "./App.css";
-import HomePage from "./pages/homepage/hompeage.componet";
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className='App'>
+      <Switch>
+        <Route component={HomePage} path='/' exact />
+      </Switch>
     </div>
   );
 }
